864d04627e88446db505aaa471f3bc6e82a44e7b
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2017-11-17 16:17:19 +0900

    moved Testdata for CrawledLens out of GHExamples because its not depending on Geizhals but general (e.g. the keys)

--- WebCrawler/CrawledLensTestsuite.py
+++ WebCrawler/CrawledLensTestsuite.py
@@ -1,5 +1,5 @@
 import unittest
-import GhExamples
+import GeneralExamples
 from CrawledLens import CrawledLens
 
 class CrawledLensTestsuite(unittest.TestCase):
@@ -14,18 +14,18 @@ class CrawledLensTestsuite(unittest.TestCase):
 
     def setUp(self):
         
-        self.__class__.CRAWLED_LENS1 = GhExamples.TESTDATA_CRAWLED_LENS1
-        self.__class__.CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT = GhExamples.TESTDATA_CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT
-        self.__class__.CRAWLED_LENS1_WITH_OLD_MOUNT = GhExamples.TESTDATA_CRAWLED_LENS1_OLD_MOUNT
-        self.__class__.CRAWLED_LENS1_WITH_NEW_MOUNT = GhExamples.TESTDATA_CRAWLED_LENS1_NEW_MOUNT
+        self.__class__.CRAWLED_LENS1 = GeneralExamples.TESTDATA_CRAWLED_LENS1
+        self.__class__.CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT = GeneralExamples.TESTDATA_CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT
+        self.__class__.CRAWLED_LENS1_WITH_OLD_MOUNT = GeneralExamples.TESTDATA_CRAWLED_LENS1_OLD_MOUNT
+        self.__class__.CRAWLED_LENS1_WITH_NEW_MOUNT = GeneralExamples.TESTDATA_CRAWLED_LENS1_NEW_MOUNT
 
-        self.__class__.CRAWLED_LENS2 = GhExamples.TESTDATA_CRAWLED_LENS2
-        self.__class__.CRAWLED_LENS2_WITHOUT_SENSOR = GhExamples.TESTDATA_CRAWLED_LENS2_WITHOUT_SENSOR
+        self.__class__.CRAWLED_LENS2 = GeneralExamples.TESTDATA_CRAWLED_LENS2
+        self.__class__.CRAWLED_LENS2_WITHOUT_SENSOR = GeneralExamples.TESTDATA_CRAWLED_LENS2_WITHOUT_SENSOR
         
-        self.__class__.CRAWLED_LENS3 = GhExamples.TESTDATA_CRAWLED_LENS3
+        self.__class__.CRAWLED_LENS3 = GeneralExamples.TESTDATA_CRAWLED_LENS3
 
-        self.__class__.ALL_CRAWLED_LENSES_WITH_MISSING_INFO = GhExamples.TESTDATA_ALL_CRAWLED_LENSES_WITH_MISSING_INFO
-        self.__class__.ALL_CRAWLED_LENSES_WITH_FULL_INFO = GhExamples.TESTDATA_ALL_CRAWLED_LENSES_WITH_FULL_INFO
+        self.__class__.ALL_CRAWLED_LENSES_WITH_MISSING_INFO = GeneralExamples.TESTDATA_ALL_CRAWLED_LENSES_WITH_MISSING_INFO
+        self.__class__.ALL_CRAWLED_LENSES_WITH_FULL_INFO = GeneralExamples.TESTDATA_ALL_CRAWLED_LENSES_WITH_FULL_INFO
 
         print("\n\nsetup Done\n" + self._testMethodName)
     #End of setUp
---
+++ WebCrawler/GeneralExamples.py
@@ -0,0 +1,102 @@
+from CrawledLens import CrawledLens
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\
+#	TESTDATA_CRAWLED_LENSES
+#/////////////////////////
+
+TESTDATA_CRAWLED_LENS1 = CrawledLens({
+	'Lensname': 'Nikon 1 NIKKOR VR 10-30mm 3.5-5.6 PD-Zoom', \
+	'Focal Length': '10-30mm', \
+	'Aperture': '1:3.5-1:5.6', \
+	'Filtersize': "", \
+	'Magnification': "", \
+	'Minimal Focus': "0.20m", \
+ 	'Mount': 'Nikon 1, Canon EF', \
+	'Sensor compatibility': 'Nikon CX', \
+ 	'Weight': '85g', \
+	'Size': '58x28mm'})
+
+TESTDATA_CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT = CrawledLens({
+	'Lensname': 'Nikon 1 NIKKOR VR 10-30mm 3.5-5.6 PD-Zoom', \
+	'Focal Length': '10-30mm', \
+	'Aperture': '1:3.5-1:5.6', \
+	'Filtersize': "", \
+	'Magnification': "", \
+	'Minimal Focus': "0.20m", \
+ 	'Mount': '', \
+	'Sensor compatibility': 'Nikon CX', \
+ 	'Weight': '', \
+	'Size': '58x28mm'})
+
+TESTDATA_CRAWLED_LENS1_OLD_MOUNT = CrawledLens({
+	'Lensname': 'Nikon 1 NIKKOR VR 10-30mm 3.5-5.6 PD-Zoom', \
+	'Focal Length': '10-30mm', \
+	'Aperture': '1:3.5-1:5.6', \
+	'Filtersize': "", \
+	'Magnification': "", \
+	'Minimal Focus': "0.20m", \
+ 	'Mount': 'Nikon 1', \
+	'Sensor compatibility': 'Nikon CX', \
+ 	'Weight': '85g', \
+	'Size': '58x28mm'})
+
+TESTDATA_CRAWLED_LENS1_NEW_MOUNT = CrawledLens({
+	'Lensname': 'Nikon 1 NIKKOR VR 10-30mm 3.5-5.6 PD-Zoom', \
+	'Focal Length': '10-30mm', \
+	'Aperture': '1:3.5-1:5.6', \
+	'Filtersize': "", \
+	'Magnification': "", \
+	'Minimal Focus': "0.20m", \
+ 	'Mount': 'Canon EF', \
+	'Sensor compatibility': 'Nikon CX', \
+ 	'Weight': '85g', \
+	'Size': '58x28mm'})
+
+TESTDATA_CRAWLED_LENS2 = CrawledLens({
+	'Lensname': 'Canon Objektiv CN-E 35mm T1.5 L F', \
+	'Focal Length': '35mm', \
+	'Aperture': '1:1.5', \
+	'Filtersize': "", \
+	'Magnification': "", \
+	'Minimal Focus': "", \
+ 	'Mount': 'Canon EF', \
+	'Sensor compatibility': 'APS-C/ Kleinbild', \
+ 	'Weight': '', \
+	'Size': ''		
+})
+TESTDATA_CRAWLED_LENS2_WITHOUT_SENSOR = CrawledLens({
+	'Lensname': 'Canon Objektiv CN-E 35mm T1.5 L F', \
+	'Focal Length': '35mm', \
+	'Aperture': '1:1.5', \
+	'Filtersize': "", \
+	'Magnification': "", \
+	'Minimal Focus': "", \
+ 	'Mount': 'Canon EF', \
+	'Sensor compatibility': '', \
+ 	'Weight': '', \
+	'Size': ''		
+})
+
+TESTDATA_CRAWLED_LENS3 = CrawledLens({
+	'Lensname': '', \
+	'Focal Length': '25mm', \
+	'Aperture': '1:0.95', \
+	'Filtersize': "52mm", \
+	'Magnification': "", \
+	'Minimal Focus': "0.17m", \
+ 	'Mount': 'Micro-Four-Thirds', \
+	'Sensor compatibility': 'Four-Thirds', \
+ 	'Weight': '410g',\
+	'Size': '58.4x70mm'
+})
+
+TESTDATA_ALL_CRAWLED_LENSES_WITH_MISSING_INFO = {
+	'Nikon 1 NIKKOR VR 10-30mm 3.5-5.6 PD-Zoom': TESTDATA_CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT,
+	'Canon Objektiv CN-E 35mm T1.5 L F': TESTDATA_CRAWLED_LENS2_WITHOUT_SENSOR
+}
+
+TESTDATA_ALL_CRAWLED_LENSES_WITH_FULL_INFO = {
+	'Nikon 1 NIKKOR VR 10-30mm 3.5-5.6 PD-Zoom': TESTDATA_CRAWLED_LENS1_WITHOUT_MOUNT_AND_WEIGHT,
+	'Canon Objektiv CN-E 35mm T1.5 L F': TESTDATA_CRAWLED_LENS2_WITHOUT_SENSOR,
+	'': TESTDATA_CRAWLED_LENS3
+}
