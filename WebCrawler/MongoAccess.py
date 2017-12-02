f2cfae09bcea16fe7bfb44c240349618f1a55940
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2017-12-02 16:02:42 +0900

    deleted start and stop, will do that myself in a seperate commandline for now, its easier.

--- WebCrawler/MongoAccess.py
+++ WebCrawler/MongoAccess.py
@@ -5,25 +5,13 @@ import DataKeys
 
 class MongoAccess:
 
-    __DB_PATH = "C:/Users/Michael/AppData/Local/MongoDB/LensComparison"
-    __MONGO_BIN = "C://Program Files//MongoDB//Server//3.4//bin//mongod"
-
     __KEY_LENSNAME = DataKeys.key_lensname_as_title
 
     def __init__(self, collection_name):
         self.__collection_name = collection_name
-        self.__mongod_process = None
-
-    def start_mongo_server(self):
-        start_command = self.__MONGO_BIN + " --dbpath " + self.__DB_PATH
-        #self.__mongod_process = subprocess.Popen(start_command)
         self.client = pymongo.MongoClient()
         self.collection = self.client["lens_db"][self.__collection_name]
-
-    def stop_mongo_server(self):
-        if(self.__mongod_process != None):
-            self.__mongod_process.terminate()
-            self.__mongod_process = None
+        
 
     def add_lens(self, lens_name, lens_dict):
         lens_name = lens_name.replace(".",",")
