61f285fda7a140221d4f84d27c8db0b292d09012
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2017-12-04 20:09:47 +0900

    restructured MongoToCsv

--- WebCrawler/MongoToCsv.py
+++ WebCrawler/MongoToCsv.py
@@ -9,6 +9,19 @@ class MongoToCsv():
     def __init__(self, mongo_db_name):
         self.mongo_db_name = mongo_db_name
 
+    def export_all_lenses(self):
+        self.__get_all_collection_names()
+        for name in self.collection_names:
+            print("Collection: " + name)
+
+        self.__gather_all_lens_dicts_of_each_domain()
+        for tmp_dict in self.domain_lens_dicts_list:
+            print("Size: " + str(len(tmp_dict)) + " Items")
+
+        self.__combine_all_lens_dicts_of_each_domain()
+        print("end size: " + str(len(self.all_integrated_lenses)))
+        self.__write_to_csv()
+
     def __get_all_collection_names(self):
         client = pymongo.MongoClient()
         self.collection_names = client[self.mongo_db_name].collection_names()
@@ -26,26 +39,43 @@ class MongoToCsv():
         #   for each key get dataset of each dict
         #   try to intergrate datasets -> add to result dict
         # If all keys are done -> return result dict
-        self.all_integrated_lenses = []
-        key_set = set()
+        self.__gather_all_domain_lens_dict_keys()
+        self.__gather_and_integrate_sources_per_lens()
+         
+
+    def __gather_all_domain_lens_dict_keys(self):
+        self.key_set = set()
 
         for domain_lens_dict in self.domain_lens_dicts_list:
             for key in domain_lens_dict.keys():
-                key_set.add(key)
+                self.key_set.add(key)
+
+    def __gather_and_integrate_sources_per_lens(self):
+        self.all_integrated_lenses = []
         
-        print("Gathering Data:")
-        for key in key_set:
-            datasets = []
-            for domain_lens_dict in self.domain_lens_dicts_list:
-                print(".", end="")
-                try:
-                    datasets.append(domain_lens_dict[key].lens_dict)
-                except KeyError:
-                    print("Unknown Key: " + key + " in: " + str(domain_lens_dict))
-            lens_integration = LensIntegration(datasets)
-            tmp_result = lens_integration.integrate()
-            self.all_integrated_lenses.append(tmp_result)
-        print("")      
+        print("Gather and Integrate Lenses:")
+        for key in self.key_set:
+            self.__gather_and_integrate_sources_for_key(key)
+        print("") 
+
+    def __gather_and_integrate_sources_for_key(self, key):
+        dataset = self.__gather_all_domain_dicts_with_key(key)
+        integrated_lens = self.__integrate_dataset(dataset)
+        self.all_integrated_lenses.append(integrated_lens)
+
+    def __gather_all_domain_dicts_with_key(self, key):
+        dataset = []
+        for domain_lens_dict in self.domain_lens_dicts_list:
+            print(".", end="")
+            try:
+                dataset.append(domain_lens_dict[key].lens_dict)
+            except KeyError:
+                print("Unknown Key: " + key + " in: " + str(domain_lens_dict))
+        return dataset
+
+    def __integrate_dataset(self, dataset):
+        lens_integration = LensIntegration(dataset)
+        return lens_integration.integrate()
 
     def __write_to_csv(self):
         file_name = "mongoRawData.csv"
@@ -57,17 +87,3 @@ class MongoToCsv():
             file.write(string + "\n")
         print("")
         file.close()
-
-
-    def export_all_lenses(self):
-        self.__get_all_collection_names()
-        for name in self.collection_names:
-            print("Collection: " + name)
-
-        self.__gather_all_lens_dicts_of_each_domain()
-        for tmp_dict in self.domain_lens_dicts_list:
-            print("Size: " + str(len(tmp_dict)) + " Items")
-
-        self.__combine_all_lens_dicts_of_each_domain()
-        print("end size: " + str(len(self.all_integrated_lenses)))
-        self.__write_to_csv()
