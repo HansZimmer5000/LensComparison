6f7fb1223a2bfdbf90f8ba1f2cac00a17a6a7a45
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2017-12-04 19:27:13 +0900

    made LensIntegration to a class and split up the big function

--- WebCrawler/LensIntegration.py
+++ WebCrawler/LensIntegration.py
@@ -1,19 +1,37 @@
 
-#Own Module since this could get very complicated.
+#Own Class since this could get very complicated.
 
-def integrate(lenses):
-    #TODO: Make better!
-    default_lens = lenses.pop(0)
-    keys = lenses[0].keys()
+class LensIntegration:
 
-    if(len(lenses) != 0):
-        for key in keys:
-            value = default_lens[key]
-            if(value == ""):
-                    for lens in lenses:
-                        value_other = lens[key]
-                        if(value != ""):
-                            default_lens[key] = value_other
+    def __init__(self, new_lenses):
+        self.default_lens = new_lenses.pop(0)
+        self.keys = self.default_lens.keys()
+        self.new_lenses = new_lenses
 
-    result_lens = default_lens
-    return result_lens
+    def integrate(self):
+        #TODO: Make better!
+
+        if(len(self.new_lenses) != 0):
+            for key in self.keys:
+                self.__set_new_value_if_existing_is_empty(key)
+
+        result_lens = self.default_lens
+        return result_lens
+
+    def __set_new_value_if_existing_is_empty(self, key):
+        old_value = self.default_lens[key]
+        if(old_value == ""):
+            self.__set_first_not_empty_new_value(key)
+
+    def __set_first_not_empty_new_value(self, key):
+        for new_lens in self.new_lenses:
+            was_set = self.__new_not_empty_data_was_set(new_lens, key)
+            if(was_set):
+                break
+
+    def __new_not_empty_data_was_set(self, new_lens, key):
+        value = new_lens[key]
+        if(value != ""):
+            self.default_lens[key] = value
+            return True
+        return False
