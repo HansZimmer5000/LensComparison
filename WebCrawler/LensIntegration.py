c316e638af67bb1614ae4d3f76dfbf7d46de29c6
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2017-12-04 20:18:24 +0900

    restructured (added __ to class variables) MongoToCsv and LensIntegration

--- WebCrawler/LensIntegration.py
+++ WebCrawler/LensIntegration.py
@@ -4,27 +4,27 @@
 class LensIntegration:
 
     def __init__(self, new_lenses):
-        self.default_lens = new_lenses.pop(0)
-        self.keys = self.default_lens.keys()
-        self.new_lenses = new_lenses
+        self.__default_lens = new_lenses.pop(0)
+        self.__keys = self.__default_lens.keys()
+        self.__new_lenses = new_lenses
 
     def integrate(self):
-        #TODO: Make better!
+        #TODO: Find more ways to integrate properly.
 
-        if(len(self.new_lenses) != 0):
-            for key in self.keys:
+        if(len(self.__new_lenses) != 0):
+            for key in self.__keys:
                 self.__set_new_value_if_existing_is_empty(key)
 
-        result_lens = self.default_lens
+        result_lens = self.__default_lens
         return result_lens
 
     def __set_new_value_if_existing_is_empty(self, key):
-        old_value = self.default_lens[key]
+        old_value = self.__default_lens[key]
         if(old_value == ""):
             self.__set_first_not_empty_new_value(key)
 
     def __set_first_not_empty_new_value(self, key):
-        for new_lens in self.new_lenses:
+        for new_lens in self.__new_lenses:
             was_set = self.__new_not_empty_data_was_set(new_lens, key)
             if(was_set):
                 break
@@ -32,6 +32,6 @@ class LensIntegration:
     def __new_not_empty_data_was_set(self, new_lens, key):
         value = new_lens[key]
         if(value != ""):
-            self.default_lens[key] = value
+            self.__default_lens[key] = value
             return True
         return False
--- WebCrawler/MongoToCsv.py
+++ WebCrawler/MongoToCsv.py
@@ -7,7 +7,7 @@ from LensIntegration import LensIntegration
 class MongoToCsv():
 
     def __init__(self, mongo_db_name):
-        self.mongo_db_name = mongo_db_name
+        self.__mongo_db_name = mongo_db_name
 
     def export_all_lenses(self):
         self.__get_all_collection_names()
@@ -15,30 +15,24 @@ class MongoToCsv():
             print("Collection: " + name)
 
         self.__gather_all_lens_dicts_of_each_domain()
-        for tmp_dict in self.domain_lens_dicts_list:
+        for tmp_dict in self.__domain_lens_dicts_list:
             print("Size: " + str(len(tmp_dict)) + " Items")
 
         self.__combine_all_lens_dicts_of_each_domain()
-        print("end size: " + str(len(self.all_integrated_lenses)))
+        print("end size: " + str(len(self.__all_integrated_lenses)))
         self.__write_to_csv()
 
     def __get_all_collection_names(self):
         client = pymongo.MongoClient()
-        self.collection_names = client[self.mongo_db_name].collection_names()
+        self.collection_names = client[self.__mongo_db_name].collection_names()
 
     def __gather_all_lens_dicts_of_each_domain(self): 
-        self.domain_lens_dicts_list = []
+        self.__domain_lens_dicts_list = []
         for collection_name in self.collection_names:
             print("Collecting from: " + collection_name)
-            self.domain_lens_dicts_list.append(MongoAccess("lens_db", collection_name).find_all_lenses())
+            self.__domain_lens_dicts_list.append(MongoAccess("lens_db", collection_name).find_all_lenses())
 
     def __combine_all_lens_dicts_of_each_domain(self):
-        # new result dict
-        # new key set
-        # get all keys of all dicts -> add to key set
-        #   for each key get dataset of each dict
-        #   try to intergrate datasets -> add to result dict
-        # If all keys are done -> return result dict
         self.__gather_all_domain_lens_dict_keys()
         self.__gather_and_integrate_sources_per_lens()
 
@@ -46,7 +40,7 @@ class MongoToCsv():
         file_name = "mongoRawData.csv"
         file = open(file_name, "w")
         print("Writing Data: ")
-        for lens_dict in self.all_integrated_lenses:
+        for lens_dict in self.__all_integrated_lenses:
             string = GhAdapter.convert_dict_to_csv_value_string(lens_dict)
             print(".", end="")
             file.write(string + "\n")
@@ -57,12 +51,12 @@ class MongoToCsv():
     def __gather_all_domain_lens_dict_keys(self):
         self.key_set = set()
 
-        for domain_lens_dict in self.domain_lens_dicts_list:
+        for domain_lens_dict in self.__domain_lens_dicts_list:
             for key in domain_lens_dict.keys():
                 self.key_set.add(key)
 
     def __gather_and_integrate_sources_per_lens(self):
-        self.all_integrated_lenses = []
+        self.__all_integrated_lenses = []
         
         print("Gather and Integrate Lenses:")
         for key in self.key_set:
@@ -72,11 +66,11 @@ class MongoToCsv():
     def __gather_and_integrate_sources_for_key(self, key):
         dataset = self.__gather_all_domain_dicts_with_key(key)
         integrated_lens = self.__integrate_dataset(dataset)
-        self.all_integrated_lenses.append(integrated_lens)
+        self.__all_integrated_lenses.append(integrated_lens)
 
     def __gather_all_domain_dicts_with_key(self, key):
         dataset = []
-        for domain_lens_dict in self.domain_lens_dicts_list:
+        for domain_lens_dict in self.__domain_lens_dicts_list:
             print(".", end="")
             try:
                 dataset.append(domain_lens_dict[key].lens_dict)
