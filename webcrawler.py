176ec0228c817d4f5cc789f03ffeeef5bb274fd4
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2017-12-11 15:51:30 +0900

    webcrawler.py now good access to the 3 main functions of the whole program

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,8 @@
+
 import sys
 import os
+import time
+from threading import Thread
 
 current_dir = os.getcwd()
 sys.path.append(current_dir + '')
@@ -7,6 +10,7 @@ sys.path.append(current_dir + '')
 from webcrawler.spiders import startcrawling
 from webcrawler.tests import testall
 from webcrawler.persistency.mongotocsv import MongoToCsv
+from webcrawler.persistency import mongoservercontrol
 
 def run_spiders():
     startcrawling.start_spider_within_python()
@@ -17,13 +21,29 @@ def run_tests():
 def export_lenses_to_csv():
     MongoToCsv("lens_db").gather_integrate_export_all_lenses()
 
+def __start_mongo_do_work_stop_mongo(function):
+    mongo_thread = __start_mongo_server()
+    time.sleep(3) #To make sure mongo_server is completly up and running.
+    try:
+        function()
+    except KeyboardInterrupt:
+        pass
+    time.sleep(1) #To make sure function and all its function are really done.
+    __stop_thread(mongo_thread)
+
+def __start_mongo_server():
+    mongo_server_thread = Thread(target = mongoservercontrol.start_mongo_server, daemon=False)
+    mongo_server_thread.start()
+    return mongo_server_thread
+
+def __stop_thread(thread):
+    os.system("taskkill /f /im mongod.exe")
+
 if __name__ == "__main__": 
     user_input = sys.argv[1]
-    print(user_input)
     if(user_input == "1"):
-        run_tests()
+        __start_mongo_do_work_stop_mongo(run_tests)
     elif(user_input == "2"):
-        run_spiders()
+        __start_mongo_do_work_stop_mongo(run_spiders)
     elif(user_input == "3"):
-        export_lenses_to_csv()
-
+        __start_mongo_do_work_stop_mongo(export_lenses_to_csv)
--- webcrawler/persistency/mongoservercontrol.py
+++ webcrawler/persistency/mongoservercontrol.py
@@ -4,7 +4,11 @@ DB_PATH = "C:/Users/Michael/AppData/Local/MongoDB/LensComparison"
 MONGO_BIN = '"C:/Program Files/MongoDB/Server/3.4/bin/mongod"'
 FUNCTION = "--dbpath"
 
-try:
-    os.system(MONGO_BIN + " " + FUNCTION + " " + DB_PATH)
-except KeyboardInterrupt:
-    print("Aborted due to User Cancel Command.")
+def start_mongo_server():
+    try:
+        os.system(MONGO_BIN + " " + FUNCTION + " " + DB_PATH)
+    except KeyboardInterrupt:
+        print("Aborted due to User Cancel Command.")
+
+if __name__ == "__main__":
+    start_mongo_server()
